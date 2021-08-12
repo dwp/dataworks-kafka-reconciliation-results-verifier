056eb911e763637a4202a8192697fc0fc62717aa
Mark <mark.stevens@infinityworks.com>
2021-08-12 11:18:35 +0100

    added example event for testing
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- tests/test_results_verifier_lambda.py
+++ tests/test_results_verifier_lambda.py
@@ -8,6 +8,10 @@ from src.results_verifier_lambda import event_handler
 
 
 class TestResultsVerifier(unittest.TestCase):
+
+    def test_entry_point(self):
+        pass
+
     def test_count_missing_exports(self):
         path = Path(os.getcwd())
         results_json_path = f"{path.parent.absolute()}/resources/results.json"
@@ -49,6 +53,74 @@ class TestResultsVerifier(unittest.TestCase):
         missing_export_count, export_count = event_handler.get_counts(null_json_record)
         self.assertEqual(missing_export_count, 0)
         self.assertEqual(export_count, 0)
+
+    def test_generate_message_payload_missing_exports(self):
+        result = event_handler.generate_message_payload(5, 100)
+        expected_result = {
+            'severity': 'Critical',
+            'notification_type': 'Error',
+            'slack_username': 'AWS Lambda Notification',
+            'title_text': 'Kafka reconciliation results',
+            'custom_elements': [
+                {'key': 'Exported count', 'value': '100'},
+                {'key': 'Missing exports count', 'value': '5'}
+            ]
+        }
+        self.assertEqual(result, expected_result)
+
+    def test_generate_message_payload_no_missing_exports(self):
+        result = event_handler.generate_message_payload(0, 50)
+        expected_result = {
+            'severity': 'High',
+            'notification_type': 'Information',
+            'slack_username': 'AWS Lambda Notification',
+            'title_text': 'Kafka reconciliation results',
+            'custom_elements': [
+                {'key': 'Exported count', 'value': '50'}
+            ]
+        }
+        self.assertEqual(result, expected_result)
+
+    @staticmethod
+    def get_event():
+        return {
+            "Records": [
+                {
+                    "eventVersion": "2.0",
+                    "eventSource": "aws:s3",
+                    "awsRegion": "eu-west-2",
+                    "eventTime": "1970-01-01T00:00:00.000Z",
+                    "eventName": "ObjectCreated:Put",
+                    "userIdentity": {
+                        "principalId": "EXAMPLE"
+                    },
+                    "requestParameters": {
+                        "sourceIPAddress": "127.0.0.1"
+                    },
+                    "responseElements": {
+                        "x-amz-request-id": "EXAMPLE123456789",
+                        "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"
+                    },
+                    "s3": {
+                        "s3SchemaVersion": "1.0",
+                        "configurationId": "testConfigRule",
+                        "bucket": {
+                            "name": "results_verifier_test",
+                            "ownerIdentity": {
+                                "principalId": "EXAMPLE"
+                            },
+                            "arn": "arn:aws:s3:::example-bucket"
+                        },
+                        "object": {
+                            "key": "query_results.json",
+                            "size": 1024,
+                            "eTag": "0123456789abcdef0123456789abcdef",
+                            "sequencer": "0A1B2C3D4E5F678901"
+                        }
+                    }
+                }
+            ]
+        }
 
     def setUp(self):
         event_handler.logger = logging.getLogger()
