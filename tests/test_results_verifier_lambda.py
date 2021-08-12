183e6b431f67a3142f0ef6aef928f84fc8981be9
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-08-12 14:44:24 +0000

    Fixup: Format Python code with Black

--- tests/test_results_verifier_lambda.py
+++ tests/test_results_verifier_lambda.py
@@ -56,28 +56,30 @@ class TestResultsVerifier(TestCase):
         null_json_record = self.get_record_containing_nulls()
         self.setUp_s3(null_json_record)
         sqs_client = boto3.client("sqs", region_name="eu-west-2")
-        sqs_url = sqs_client.create_queue(QueueName='test')["QueueUrl"]
-        sqs_arn = sqs_client.get_queue_attributes(QueueUrl=sqs_url)["Attributes"]["QueueArn"]
+        sqs_url = sqs_client.create_queue(QueueName="test")["QueueUrl"]
+        sqs_arn = sqs_client.get_queue_attributes(QueueUrl=sqs_url)["Attributes"][
+            "QueueArn"
+        ]
         sns_topic_arn = self.setup_sns(sqs_arn)
 
         event = self.get_event()
         with mock.patch.dict(
-                os.environ, {"SNS_TOPIC": sns_topic_arn, "AWS_REGION": "eu-west-2"}
+            os.environ, {"SNS_TOPIC": sns_topic_arn, "AWS_REGION": "eu-west-2"}
         ):
             event_handler.handler(event, None)
-            messages = sqs_client.receive_message(QueueUrl=sqs_url, MaxNumberOfMessages=10)["Messages"]
+            messages = sqs_client.receive_message(
+                QueueUrl=sqs_url, MaxNumberOfMessages=10
+            )["Messages"]
             self.assertEqual(1, len(messages))
-            message_body = json.loads(messages[0]['Body'])
-            message = json.loads(message_body['Message'])
+            message_body = json.loads(messages[0]["Body"])
+            message = json.loads(message_body["Message"])
 
             expected_message = {
                 "severity": "High",
                 "notification_type": "Information",
                 "slack_username": "AWS Lambda Notification",
                 "title_text": "Kafka reconciliation successful",
-                "custom_elements": [
-                    {"key": "Exported count", "value": "0"}
-                ]
+                "custom_elements": [{"key": "Exported count", "value": "0"}],
             }
 
             self.assertEqual(message, expected_message)
